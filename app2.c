019a201d2d98e69ff0b01ebdf9062ed0856da27a
oguyon <oliv.guyon@gmail.com>
2017-10-28 12:06:26 -1000

    sync

---
+++ app2.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "module00/module00.h"
+#include "module01/module01.h"
+#include "module02/module02.h"
+
+int main()
+{
+printf("useless main\n");
+
+return 0;
+
+}
